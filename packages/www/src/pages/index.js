37079f262295f58db84f82dfc9d7934ac0857739
svirins <svirins@gmail.com>
2020-02-28 12:00:23 +0300

    auth

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Heading, Button, Flex } from "theme-ui";
-export default props => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-      <Heading as="h1">Get Stuff done</Heading>
-      <Button
-        sx={{ marginTop: 2 }}
-        onClick={() => {
-          alert("Clicked");
-        }}
-      >
-        Log in
-      </Button>
-    </Flex>
-  </Container>
-);
+import netlifyIdentity from "netlify-identity-widget";
+
+export default props => {
+  useEffect(() => {
+    netlifyIdentity.init({});
+  });
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">Get Stuff done</Heading>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            netlifyIdentity.open();
+          }}
+        >
+          Log in
+        </Button>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            console.log(netlifyIdentity.currentUser());
+          }}
+        >
+          Log User
+        </Button>
+      </Flex>
+    </Container>
+  );
+};
