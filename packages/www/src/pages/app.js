eb4c58018513b9b52bea9c0136216f12b0d72b8d
svirins <svirins@gmail.com>
2020-02-28 20:07:06 +0300

    add serverless

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -1,43 +1,26 @@
 import React, { useContext } from "react";
 import { Router, Link } from "@reach/router";
 import { IdentityContext } from "../../identity-context";
-import { Heading, Button, Flex, NavLink } from "theme-ui";
-
-let Dash = () => {
-  const { user } = useContext(IdentityContext);
-  return <div>Dash hasUser: {user && user.user_metadata}</div>;
-};
+import { Container, Flex, Heading, Button, NavLink } from "theme-ui";
+import Dash from "../components/dashboard";
 
 let DashLoggedOut = props => {
   const { user, identity: netlifyIdentity } = useContext(IdentityContext);
 
   return (
-    <Flex>
-      <Flex as="nav">
-        <NavLink as={Link} to="/" p={2}>
-          Home
-        </NavLink>
-        <NavLink as={Link} to="/app" p={2}>
-          Dashboard
-        </NavLink>
-        {user && (
-          <NavLink href="#!" p={2}>
-            {user.user_metadata.full_name}
-          </NavLink>
-        )}
-      </Flex>
+    <Container>
       <Flex sx={{ flexDirection: "column", padding: 3 }}>
-        <Heading as="h1">Get Stuff done</Heading>
+        <Heading as="h1">Get Stuff Done</Heading>
         <Button
           sx={{ marginTop: 2 }}
           onClick={() => {
             netlifyIdentity.open();
           }}
         >
-          Log in
+          Log In
         </Button>
       </Flex>
-    </Flex>
+    </Container>
   );
 };
 
