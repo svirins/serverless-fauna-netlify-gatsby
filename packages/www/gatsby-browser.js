90d83ef656a85318433c5b6305cfb561a3e35695
svirins <svirins@gmail.com>
2020-03-01 18:46:03 +0300

    +gql

--- packages/www/gatsby-browser.js
+++ packages/www/gatsby-browser.js
@@ -1,4 +1,23 @@
-const React = require("react")
-const wrapRootElement = require('./wrap-root-element')
+const React = require("react");
+const wrapRootElement = require("./wrap-root-element");
+const {
+  ApolloProvider,
+  ApolloClient,
+  HttpLink,
+  InMemoryCache
+} = require("@apollo/client");
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri:
+      "https://serverless-fauna-netlify-gatsby.netlify.com/.netlify/functions/graphql"
+  })
+});
 
-exports.wrapRootElement = wrapRootElement
+exports.wrapRootElement = ({ element }) => {
+  return (
+    <ApolloProvider client={client}>
+      {wrapRootElement({ element })}
+    </ApolloProvider>
+  );
+};
--- packages/www/wrap-root-element.js
+++ packages/www/wrap-root-element.js
@@ -2,28 +2,14 @@ const React = require("react");
 const { ThemeProvider } = require("theme-ui");
 const { deep } = require("@theme-ui/presets");
 const { Provider } = require("./identity-context");
-const {
-  ApolloProvider,
-  ApolloClient,
-  HttpLink,
-  InMemoryCache
-} = require("@apollo/client");
+
 const tokens = {
   ...deep,
   sizes: { container: 1024 }
 };
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri:
-      "https://serverless-fauna-netlify-gatsby.netlify.com/.netlify/functions/graphql"
-  })
-});
 
 module.exports = ({ element }) => (
-  <ApolloProvider client={client}>
     <Provider>
       <ThemeProvider theme={tokens}>{element}</ThemeProvider>
     </Provider>
-  </ApolloProvider>
 );
